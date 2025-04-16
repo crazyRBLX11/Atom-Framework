97b5f68ca313a9c0968435c3dba1b958c90ace1a
crazyRBLX11 <165557662+crazyRBLX11@users.noreply.github.com>
2025-04-16 14:58:29 +0100

    Atom.lua has been updated to use the new AtomServer and AtomClient names.

--- src/Atom.lua
+++ src/Atom.lua
@@ -1,12 +1,12 @@
 local RunService = game:GetService("RunService")
 
 if RunService:IsServer() then
-	return require(script:FindFirstChild("AtomServer_OOP", true))
+	return require(script:FindFirstChild("AtomServer", true))
 else
-	local AtomServer = script:FindFirstChild("AtomServer_OOP", true)
+	local AtomServer = script:FindFirstChild("AtomServer", true)
 	if AtomServer and RunService:IsRunning() then
 		AtomServer:Destroy()
 	end
 
-	return require(script:FindFirstChild("AtomClient_OOP", true))
+	return require(script:FindFirstChild("AtomClient", true))
 end
